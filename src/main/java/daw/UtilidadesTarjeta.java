ef1c1fa20c8db2d45a2c8d683d6c45fd83f75ad4
None083 <perezgonzaleznoelia08@gmail.com>
2024-01-18 12:46:19 +0100

    Sigue método tarjeta valida

--- src/main/java/daw/UtilidadesTarjeta.java
+++ src/main/java/daw/UtilidadesTarjeta.java
@@ -12,38 +12,46 @@ import java.util.ArrayList;
  * @author francisco
  */
 public class UtilidadesTarjeta {
-    
-    public ArrayList<Tarjeta> baseDatosTarjeta(){
+
+    public ArrayList<Tarjeta> baseDatosTarjeta() {
         ArrayList<Tarjeta> listaTarjetas = new ArrayList<>();
-        
-        listaTarjetas.add(new Tarjeta("1234567891234567", 
-                LocalDate.of(2026, 10, 1), 
+
+        listaTarjetas.add(new Tarjeta("1234567891234567",
+                LocalDate.of(2026, 10, 1),
                 "123", "Noelia", 432));
-        
+
         //Poco saldo
-        listaTarjetas.add(new Tarjeta("1234567898912345", 
-                LocalDate.of(2024, 3, 10), 
+        listaTarjetas.add(new Tarjeta("1234567898912345",
+                LocalDate.of(2024, 3, 10),
                 "456", "Fran", 10));
-        
+
         //Caducada
-        listaTarjetas.add(new Tarjeta("1234567894567891", 
-                LocalDate.of(2023, 12, 28), 
+        listaTarjetas.add(new Tarjeta("1234567894567891",
+                LocalDate.of(2023, 12, 28),
                 "789", "Vica", 100000));
-        
+
         return listaTarjetas;
     }
-    
-    public boolean numTarjetaValido(int digitosCliente){
-        boolean esValida = false;
+
+    public boolean numTarjetaValido(String digitosCliente) {
         ArrayList<Tarjeta> baseDatosTarjeta = baseDatosTarjeta();
         
-        for (int i = 0; i < baseDatosTarjeta.size(); i++) {
-            baseDatosTarjeta.get(i).getNumTarjeta();
+        if(digitosCliente){
             
         }
-        
-        
-        return esValida;
+        for (int i = 0; i < baseDatosTarjeta.size(); i++) {
+
+            if (digitosCliente.equals(baseDatosTarjeta.get(i)
+                    .getNumTarjeta()
+                    .substring(baseDatosTarjeta.get(i).getNumTarjeta().length() - 4,
+                            baseDatosTarjeta.get(i).getNumTarjeta().length()))) {
+                return true;
+            }
+        }
+
+        return false;
     }
     
+    
+
 }
